423f046a7c7cc6b60f5d88aca9ea14563254d6d4
lionel <lisi_xdu@126.com>
2016-12-27 09:58:40 +0800

    commit fsm demo

---
+++ fsm demo/src/com/aiBeginner/entity/LocationType.java
@@ -0,0 +1,21 @@
+/**   
+* @Title: LocationType.java 
+* @Package com.aiBeginner.entity 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author lisi_stuart https://github.com/lionelStuart   
+* @date 2016年12月26日 下午3:24:02 
+* @version V1.0   
+*/
+package com.aiBeginner.entity;
+
+/** 
+* @ClassName: LocationType 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author A18ccms a18ccms_gmail_com 
+* @date 2016年12月26日 下午3:24:02 
+*  
+*/
+public enum LocationType {
+	LOCATION_GOLD_MINE,
+	LOCATION_NULL;
+}
